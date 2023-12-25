be9752ff02b2eb9bdafcc8e56c0ec0ab724fdeb0
Gustavo Nor <gustavo0951@gmail.com>
2023-12-25 18:23:52 -0300

    Initial commit

---
+++ src/main/java/com/consultatabelafipe/ConsultaTabelaFipe/ConsultaTabelaFipeApplication.java
@@ -0,0 +1,13 @@
+package com.consultatabelafipe.ConsultaTabelaFipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaTabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaTabelaFipeApplication.class, args);
+	}
+
+}
